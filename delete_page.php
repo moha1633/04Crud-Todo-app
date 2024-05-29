d4fdcb4184af3bfab7487dea0a935fdc6f6f2383
mohamed abdi <mgudaal6@gmail.com>
2024-05-29 18:18:59 +0000

    added some changed with delete

--- delete_page.php
+++ delete_page.php
@@ -1,31 +1,23 @@
 <?php
-ob_start();  // Starta outputbuffring
+ob_start();
+include('dbcon.php');
 
-include('dbcon.php');  // Inkludera databasanslutningen från dbcon.php
-
-if(isset($_GET['id'])){  // Kontrollera om 'id' finns i URL-parametrarna
-    $id = $_GET['id'];  // Tilldela värdet av 'id' till variabeln $id
+if(isset($_GET['id'])){
+    $id = $_GET['id'];
 
     try {
-        // Förbered SQL-frågan för att radera en post baserad på 'id'
-        $stmt = $conn->prepare("DELETE FROM `04Crud-Todo-app` WHERE id = :id");
-        
-        // Bind 'id'-parametern till SQL-frågan
+        $stmt = $conn->prepare("DELETE FROM `todos` WHERE id = :id");
         $stmt->bindParam(':id', $id, PDO::PARAM_INT);
         
-        // Kör SQL-frågan
         if($stmt->execute()){
-            // Om raderingen lyckas, omdirigera till index.php med ett meddelande
-            header('location:index.php?delete_msg=you have deleted the record.');
+            header('location:index.php?delete_msg=Task deleted successfully');
         } else {
-            // Om SQL-frågan misslyckas, avsluta scriptet med ett felmeddelande
             die("query failed");
         }
     } catch(PDOException $e) {
-        // Hantera undantag och visa felmeddelande
         echo "Error: " . $e->getMessage();
     }
 }
 
-ob_end_flush();  // Flöda (skicka) den buffrade outputen
+ob_end_flush();
 ?>
